55b34e73042a5917f707b532156deeab0d139370
javinnieto <javiernieto.ni@gmail.com>
2025-09-04 01:49:28 +0000

    fix: mas puertos y cosas de docker-compose

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-aqtk04-3-ky%o3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.10.0.3', 'backend']
 
 
 # Application definition
